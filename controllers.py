2d09fb9ca65e6639b75c07c493a8e4e9267dc859
Raúl <rsaaperez@danielcastelao.org>
2015-04-01 17:50:20 +0200

    Initial commit

---
+++ controllers.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from openerp import http
+
+class Academy(http.Controller):
+    @http.route('/academy/academy/', auth='public')
+    def index(self, **kw):
+        Teachers = http.request.env['academy.teachers']
+        return http.request.render('academy.index', {
+            'teachers': Teachers.search([])
+        })
